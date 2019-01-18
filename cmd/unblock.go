89c3067badd8c7e7030ab7772e51047b71288eff
Daniel Morandini <danielmorandini@me.com>
2019-01-18 14:08:20 +0100

    Add unblock command

---
+++ cmd/unblock.go
@@ -0,0 +1,61 @@
+// Copyright © 2019 KIM KeepInMind GmbH/srl
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// unblockCmd represents the unblock command
+var unblockCmd = &cobra.Command{
+	Use:   "unblock",
+	Short: "Make booster unblock the sources specified",
+	Long: `Perform an HTTP request to "/block.json" for each source specified, making
+booster remove a block policy on it, i.e. the source will again be used.
+Outputs the errors returned if any.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		for _, v := range args {
+			unblock(v)
+		}
+	},
+}
+
+func unblock(source string) {
+	resp, err := cl.Del("/sources/"+source+"/block.json")
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+		return
+	}
+
+	defer resp.Body.Close()
+	fmt.Printf("Unblocked: %s\n", source)
+}
+
+func init() {
+	rootCmd.AddCommand(unblockCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// unblockCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// unblockCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
