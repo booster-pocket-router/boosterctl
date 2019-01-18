79e81447a0c32843c172299aeecb9bb818ed8b9a
Daniel Morandini <danielmorandini@me.com>
2019-01-18 12:20:15 +0100

    Add get policies command

---
+++ cmd/policies.go
@@ -0,0 +1,61 @@
+// Copyright © 2019 KIM KeepInMind GmbH/srl
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/booster-proj/booster.cli/client"
+	"github.com/spf13/cobra"
+)
+
+// policiesCmd represents the policies command
+var policiesCmd = &cobra.Command{
+	Use:   "policies",
+	Short: "Show the current list of sources",
+	Long: `Perform an HTTP request to "/policies.json" and prints the raw
+JSON returned by the API, fixing indentation for improved readability.
+The content returned contains information about the list of policies that booster
+is currenlty using.
+Outputs the error returned if any.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		resp, err := cl.Get("/policies.json", nil)
+		if err != nil {
+			fmt.Printf("Error: %v\n", err)
+			return
+		}
+
+		defer resp.Body.Close()
+		if err := client.PrettyJSON(resp.Body, os.Stderr); err != nil {
+			fmt.Printf("Error: %v\n", err)
+		}
+	},
+}
+
+func init() {
+	getCmd.AddCommand(policiesCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// policiesCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// policiesCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
